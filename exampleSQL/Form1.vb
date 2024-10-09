c8af394be98e36e01943472a2312b5bd75a2acd8
jasaga2401 <jasaga@protonmail.com>
2024-10-10 00:21:17 +0100

    SQL Injection prevention techniques

--- exampleSQL/Form1.vb
+++ exampleSQL/Form1.vb
@@ -1,44 +1,62 @@
 ﻿
+' uses a user with changed priviledges to limit their access to the database
+
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel
 Imports MySql.Data.MySqlClient
 
 Public Class Form1
     Private Sub btnEnter_Click(sender As Object, e As EventArgs) Handles btnEnter.Click
 
-        Dim connectionString As String = "Server=localhost;Database=dbUser;User ID=root;Password=12Yellow34!;"
+        Dim connectionString As String = "Server=localhost;Database=dbuser;User ID=newuser;Password=12Yellow34!"
 
-        ' captured username / password gtom the form
-        Dim enteredUsername As String = txtUsername.Text
-        Dim enteredPassword As String = txtPassword.Text
+        ' Capture the username and password from the form
+        Dim username As String = txtUsername.Text
+        Dim passwd As String = txtPassword.Text
 
-        ' SQL Query to check for users from tblUser table 
-        ' The query is vulnerable to SQL Injection as there are no prepared statements
-        ' ExampleS:  
-        '    ' OR 1=1;
-        '    ' OR 1=1 -- -
-        '    ' OR 1=1; DELETE FROM tbluser;
-        '    ' OR 1=1; DROP TABLE 
-        '    ' OR 1=1; DROP DATABASE dbuser;
-        '    ' OR 1=1; UPDATE tbluser SET passwd = 'newpassword' WHERE username = 'admin';
+        ' ----------------- SQL Protection 1 ----------------- (sanitised input)
+        ' Sanitising against text inputs protects from SQL injection attacks
 
-        ' Any number of SQL statements could be injected once the dtaabase is vulnerable
+        ' sanitising the text inputs
+        username = username.Replace("'", "").Replace(";", "")
+        passwd = passwd.Replace("'", "").Replace(";", "")
 
-        Dim query As String = "SELECT username, passwd FROM tbluser WHERE username = '" & enteredUsername & "' AND passwd = '" & enteredPassword & "'"
+        ' Using regular expressions to sanitise the text inputs
+        If Not System.Text.RegularExpressions.Regex.IsMatch(username, "^[a-zA-Z0-9]+$") Then
+            MessageBox.Show("Invalid characters in username")
+            Exit Sub
+        End If
 
-        ' Create a MySQL connection object
+        If Not System.Text.RegularExpressions.Regex.IsMatch(passwd, "^[a-zA-Z0-9]+$") Then
+            MessageBox.Show("Invalid characters in password")
+            Exit Sub
+        End If
+
+
+        ' ----------------- SQL Protection 2 ----------------- (parameterised queries)
+        ' SQL Query with parameters
+        ' Dim query As String = "SELECT * FROM tblUser WHERE username = @username AND password = @password"
+
+        ' Use of stored procedure with parameters to execute a query
+        Dim query As String = "CALL sp_ValidateUser1(@username, @password)"
+
+        ' Create a new MySQL connection
         Using conn As New MySqlConnection(connectionString)
             Try
-                ' Open the connection
                 conn.Open()
 
+                ' Use a MySQL command to execute the query
                 Using cmd As New MySqlCommand(query, conn)
+
+                    ' Add parameters to prevent SQL injection
+                    cmd.Parameters.AddWithValue("@username", username)
+                    cmd.Parameters.AddWithValue("@password", passwd)
+
                     ' Execute the query and read the results
                     Using reader As MySqlDataReader = cmd.ExecuteReader()
                         If reader.HasRows Then
                             ' Valid user found
                             While reader.Read()
                                 MessageBox.Show("User authenticated: " & reader("username").ToString())
-                                frmMenu.Show()
-                                Me.Hide()
                             End While
                         Else
                             ' No valid user found
@@ -48,27 +66,14 @@ Public Class Form1
                 End Using
 
             Catch ex As MySqlException
-                ' Handle any database errors
-                Console.WriteLine("Error: " & ex.Message)
+                MessageBox.Show("Database error: " & ex.Message)
             Finally
-                ' Close the connection
                 conn.Close()
             End Try
         End Using
 
-        ' Keep the console open
-        Console.ReadLine()
-
     End Sub
 
-    Function ValidateUser(ByVal enteredUsername As String, ByVal username As String, ByVal enteredPassword As String, ByVal passwd As String) As Boolean
-        ' Example logic: let's say a valid user has "admin" as a username
 
-        If enteredUsername = username And enteredPassword = passwd Then
-            Return True
-        Else
-            Return False
-        End If
-    End Function
 
 End Class
